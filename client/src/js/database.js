c396cfe57493c07aef2d43a569edd4d4fc81b159
restas13 <72560360+restas13@users.noreply.github.com>
2023-08-06 22:04:49 -0700

    adding the get method in the database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,16 +15,33 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jate_db = await openDB('jate', 1);
+  // Putting requires a read and write transaction
   const trans = jate_db.transaction('jate', 'readwrite');
   const store = trans.objectStore('jate');
   const req = store.put({ id: 1, value: content });
   const res = await req;
 
-  
+
   console.log(res.value);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jate_db = await openDB('jate', 1);
+  // a get request only requires a read transaction
+  const trans = jate_db.transaction('jate', 'readonly');
+  const store = trans.objectStore('jate');
+  const req = store.get(1);
+  const res = await req;
+
+  if (req) {
+    console.log(res.value);
+  }else {
+    console.log('Error getting data');
+  }
+
+  return res.value;
+
+};
 
 initdb();
